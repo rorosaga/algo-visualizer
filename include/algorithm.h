e6a3c1ebcfcc37647d5d31ee7cbd75b5bd361d39
rorosaga <rodrigo.sagastegui9@gmail.com>
2024-11-04 22:59:06 +0100

    added tests for sortalgos and doxygen comments

--- include/algorithm.h
+++ include/algorithm.h
@@ -4,17 +4,46 @@
 #include <stack>
 
 namespace algorithm {
-
+    /**
+     * @brief Sorts the given array using the selection sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void selectionSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
+    /**
+     * @brief Sorts the given array using the bubble sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void bubbleSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
+    /**
+     * @brief Sorts the given array using the insertion sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void insertionSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
+    /**
+     * @brief Sorts the given array using the merge sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void mergeSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
+    /**
+     * @brief Sorts the given array using the quick sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void quickSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
+    /**
+     * @brief Sorts the given array using the bogo sort algorithm
+     * @param arr The array to sort
+     * @param stepCallback lambda function to call after each step letting the caller know the current state of the array
+     */
     void bogoSort(std::vector<int>& arr, std::function<void(const std::vector<int>&)> stepCallback = [](const std::vector<int>&) {});
 
 }
