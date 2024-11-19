2241204610af34221e13a6fc3616067681fb4e95
ramcav <110005362+ramcav@users.noreply.github.com>
2024-11-19 12:31:34 +0100

    Added lambda to see how the built-in sorting works

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,18 @@ int main() {
     
     algorithm::randomNumberGen(arr, 300, 0, 100);
 
-    SortVisualizer visualizer(1000, 1000, arr.size(), 50, "Merge Sort", "O(n log n)");
+    SortVisualizer visualizer(1000, 1000, arr.size(), 300, "Merge Sort", "O(n log n)");
 
     algorithm::iterMergeSort(arr, [&sortingSteps](const std::vector<int>& step){
         sortingSteps.push_back(step);
     });
+
+    
+
+    // std::sort(arr.begin(), arr.end(), [&sortingSteps, &arr](int a, int b) {
+    //     sortingSteps.push_back(arr);
+    //     return a < b;
+    // });
 
     for (const auto& step : sortingSteps) {
         visualizer.addState(step);
